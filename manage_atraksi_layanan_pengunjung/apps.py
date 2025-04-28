92504bb6f62fe9a648ddddfd8bc6914dad8fcda9
nsrnannaisha <nisrinaannaisha@gmail.com>
2025-04-28 13:50:12 +0700

    add module manage_atraksi_layanan_pengunjung

---
+++ manage_atraksi_layanan_pengunjung/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ManageAtraksiLayananPengunjungConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'manage_atraksi_layanan_pengunjung'

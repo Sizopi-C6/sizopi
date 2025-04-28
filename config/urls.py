ce05e0ebeff64bb4740d4a79cad96e0259b5972c
FransicaEllyaB <ellyafransiscab@gmail.com>
2025-04-28 20:18:20 +0700

    chore: add style, view, and url habitat satwa

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('data_satwa_habitat.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ data_satwa_habitat/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from data_satwa_habitat.views import *
+
+app_name = 'data_satwa_habitat'
+
+urlpatterns = [
+    path('', show_main, name='show_main'),
+    path('tambah-satwa/', tambah_satwa, name='tambah_satwa'),
+    path('list-satwa/', show_list_satwa, name='show_list_satwa'),
+    path('edit-satwa/<uuid:id>/', edit_satwa, name='edit_satwa'),
+    path('delete-satwa/<uuid:id>/', delete_satwa, name='delete_satwa'),
+
+    path('habitat/', list_habitat, name='list_habitat'),
+    path('tambah-habitat/', tambah_habitat, name='tambah_habitat'),
+    path('edit-habitat/<str:habitat_nama>/', edit_habitat, name='edit_habitat'),
+    path('habitat/<str:habitat_nama>/', detail_habitat, name='detail_habitat'),
+    path('delete-habitat/<str:habitat_nama>/', delete_habitat, name='delete_habitat'),
+]

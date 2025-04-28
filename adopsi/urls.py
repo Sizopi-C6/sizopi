282ae4aeeccdf1a6de8620d14a1a30a09c4fe7d2
deanitasekar <deanitasekar@gmail.com>
2025-04-29 00:50:55 +0700

    feat: add view & url for Manajemen Data Adopsi

--- adopsi/urls.py
+++ adopsi/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import program_adopsi_admin
+from .views import program_adopsi_admin, program_adopsi_pengunjung
 
 app_name = 'adopsi'
 urlpatterns = [
     path('admin/', program_adopsi_admin, name='adopsi_admin'),
+    path('', program_adopsi_pengunjung, name='adopsi_pengunjung'),
 ]
--- adopsi/views.py
+++ adopsi/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def program_adopsi_admin(request):
     return render(request, 'adopsi_admin.html')
+
+def program_adopsi_pengunjung(request):
+    return render(request, 'adopsi_pengunjung.html')
